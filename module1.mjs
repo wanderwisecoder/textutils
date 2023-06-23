bd567bdd45fac565829aff8764eca7083872712b
wanderwisecoder <thissurajneversets@gmail.com>
2023-06-23 19:02:58 +0530

    Add existing project files to Git

--- module1.mjs
+++
@@ -1,6 +0,0 @@
-import ui, { b, a, c } from './module2.mjs';
-console.log(ui);
-console.log(b);
-
-console.log(a);
-console.log(c);
--- module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = 'Suraj';
-const b = 'Zoro';
-const c = 'Luffy';
-const d = 'Chopper';
-
-export default d;
-export { b };
-export { c };
-export { a };
